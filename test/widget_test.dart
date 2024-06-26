30c03e140e845d399e01454a3bc77ea0dd10bc6f
alfihzm <alfihzm@gmail.com>
2024-06-26 23:02:56 +0700

    Final commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:resep_nusantara/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const kulinerNusantara());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
